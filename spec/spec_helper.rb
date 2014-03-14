724cf07e5fa48dbc657b55c8b61be9fbe512c7a0
Diego Carrion <dc.rec1@gmail.com>
2014-03-14 17:06:34 -0300

    configured abacos for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'abacos'
+require 'pry'
 
 RSpec.configure do |config|
-
 end
+
+Abacos.config! token: File.read('spec/token').strip, sandbox: true
