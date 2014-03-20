90d38fe5d324187cc29a100d4f9ed6c30549996d
Diego Carrion <dc.rec1@gmail.com>
2014-03-19 21:12:15 -0300

    configured vcr with webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,13 @@ require 'rubygems'
 require 'bundler/setup'
 require 'abacos'
 require 'pry'
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+end
 
 RSpec.configure do |config|
 end
