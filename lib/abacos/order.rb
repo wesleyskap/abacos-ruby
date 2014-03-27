a377a5c88977611f5d087c25401b480d66c56a84
Diego Carrion <dc.rec1@gmail.com>
2014-03-27 16:01:02 -0300

    updated Order.create! syntax

--- lib/abacos/order.rb
+++ lib/abacos/order.rb
@@ -2,8 +2,8 @@ module Abacos
   class Order < Base
     self.ws = "Pedidos"
 
-    def self.create!(*params)
-      call :inserir_pedido, lista_de_pedidos: params.map { |param| { dados_pedidos: param }}
+    def self.create!(*params, chave_identificacao:)
+      call :inserir_pedido, { chave_identificacao: chave_identificacao }.merge(lista_de_pedidos: params.map { |param| { dados_pedidos: param }})
     end
   end
 end
