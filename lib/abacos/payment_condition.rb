7acca8ecfb099374a06091f427eb7d93a14cc3af
Diego Carrion <dc.rec1@gmail.com>
2014-03-31 22:49:34 -0300

    changed PaymentCondition.find_all_by_cnpj for PaymentCondition.all

--- lib/abacos/payment_condition.rb
+++ lib/abacos/payment_condition.rb
@@ -2,8 +2,8 @@ module Abacos
   class PaymentCondition < Base
     attr_reader :id, :name
 
-    def self.find_all_by_cnpj(cnpj)
-      execute("select id = tcom_conpag.conp_cod, name = rtrim(tcom_conpag.conp_nom) from abacos..tgen_entbas, abacos..tcom_clifor, abacos..tcom_crcpag, abacos..tcom_conpag where tgen_entbas.entb_cod = tcom_clifor.entb_cod and tcom_clifor.clif_cod = tcom_crcpag.clif_cod and tcom_crcpag.conp_cod = tcom_conpag.conp_cod and tgen_entbas.entb_cgc = '#{cnpj}' order by tcom_conpag.conp_nom")
+    def self.all
+      execute("select id = conp_cod, name = rtrim(conp_nom) from abacos..tcom_conpag order by conp_nom")
     end
   end
 end
--- spec/abacos/payment_condition_spec.rb
+++ spec/abacos/payment_condition_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Abacos::PaymentCondition do
   describe ".find_all_by_cnpj" do
     it "should return all payment conditions" do
-      payment_conditions = Abacos::PaymentCondition.find_all_by_cnpj('15436940000286')
+      payment_conditions = Abacos::PaymentCondition.all
       payment_conditions.last.name.should == "A VISTA"
       payment_conditions.first.id.should == 12
     end
