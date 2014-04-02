ed2d680d8995afeaf8e3206757c30a63f98e1bbc
Diego Carrion <dc.rec1@gmail.com>
2014-04-02 19:59:00 -0300

    removed puts

--- spec/abacos/customer_spec.rb
+++ spec/abacos/customer_spec.rb
@@ -5,7 +5,6 @@ describe Abacos::Customer do
     it "should create a customer and return the response" do
       VCR.use_cassette('customer_success') do
         customer = Abacos::Customer.create!({ 'CPFouCNPJ' => '15436940000286', tipo_pessoa: 'tpeFisica', sexo: 'tseMasculino', nome: 'Ronaldo', telefone: '45225842', celular: '73495254', endereco: { logradouro: 'Rua Peri, 100', complemento_endereco: 'casa azul', bairro: 'Centro', municipio: 'Brasília', estado: 'SP', cep: '70675100' }}, { chave_identificacao: ABACOS_CONFIG['token'] })
-        puts customer.response.to_s
         customer.should be_persisted
         customer.response.to_s.should start_with("<?xml version")
       end
