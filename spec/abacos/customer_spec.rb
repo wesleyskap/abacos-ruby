45aa7e836252b487738ceb08f820ff3f0fd6bad4
Diego Carrion <dc.rec1@gmail.com>
2014-04-02 13:10:10 -0300

    added Customer.create\!

--- spec/abacos/customer_spec.rb
+++ spec/abacos/customer_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Abacos::Customer do
   describe ".create!" do
-    xit "should create a customer and return the response" do
+    it "should create a customer and return the response" do
       VCR.use_cassette('customer_success') do
-        customer = Abacos::Customer.create!({ 'CPFouCNPJ' => '15436940000286', tipo_pessoa: 'tpeFisica', sexo: 'tseMasculino', nome: 'Ronaldo', telefone: '45225842', celular: '73495254', dados_endereco: { logradouro: 'Rua Peri, 100', complemento_endereco: 'casa azul', bairro: 'Centro', municipio: 'Brasília', estado: 'SP', cep: '70675100' }}, { chave_identificacao: ABACOS_CONFIG['token'] })
+        customer = Abacos::Customer.create!({ 'CPFouCNPJ' => '15436940000286', tipo_pessoa: 'tpeFisica', sexo: 'tseMasculino', nome: 'Ronaldo', telefone: '45225842', celular: '73495254', endereco: { logradouro: 'Rua Peri, 100', complemento_endereco: 'casa azul', bairro: 'Centro', municipio: 'Brasília', estado: 'SP', cep: '70675100' }}, { chave_identificacao: ABACOS_CONFIG['token'] })
         puts customer.response.to_s
         customer.should be_persisted
         customer.response.to_s.should start_with("<?xml version")
