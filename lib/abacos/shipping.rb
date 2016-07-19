e380c53488f9f5a0ee4e0ed41bb854a2525f66ce
Wesley Conde <wesleyskap@gmail.com>
2016-07-19 12:36:16 -0300

    update shipping select

--- lib/abacos/shipping.rb
+++ lib/abacos/shipping.rb
@@ -3,9 +3,7 @@ module Abacos
     attr_reader :price, :name
 
     def self.find_all_by_cep_and_weight cep, weight
-      execute("select name = SE.SERE_NOM, price = round(min(fr.FREP_VAL_COB)*1.10, 2) from abacos..tcom_locent lo with (nolock) inner join abacos..TCOM_FREPES fr with (nolock) on lo.LOCE_COD=fr.LOCE_COD inner join abacos..TCOM_SERENT se with (nolock) on lo.SERE_COD=se.SERE_COD where '#{cep}' between LOCE_CEP_ini and loce_cep_fim and FREP_PES_MAX  >= #{weight} and se.TRAN_COD=1 and se.SERE_COD in(3,2) group by SE.SERE_NOM")
+      execute("SELECT name=SE.SERE_NOM, price=round(min(fr.FREP_VAL_COB)*1.035,2) FROM ABACOS.dbo.tcom_locent lo WITH (NOLOCK) INNER JOIN ABACOS.dbo.TCOM_FREPES fr WITH (NOLOCK) ON lo.LOCE_COD=fr.LOCE_COD INNER JOIN ABACOS.dbo.TCOM_SERENT se WITH (NOLOCK) ON lo.SERE_COD=se.SERE_COD WHERE  '#{cep}' BETWEEN lo.LOCE_CEP_INI AND lo.LOCE_CEP_FIM AND fr.FREP_PES_MAX >= #{weight} AND se.TRAN_COD=1 AND se.SERE_COD IN (1,2,3) AND fr.FREP_VAL_COB>0 GROUP BY SE.SERE_NOM ORDER BY price ASC")
     end
   end
 end
-
-
